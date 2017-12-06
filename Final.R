ab83b0de68f8c6c3c7f82b73f16597145ffd8baf
YouYouDaWang <31570500+YouYouDaWang@users.noreply.github.com>
2017-12-06 15:30:27 -0500

    Add files via upload

---
+++ Final.R
@@ -0,0 +1,33 @@
+library(data.table)
+library(dplyr)
+
+#R data frames and file reading and writing
+
+
+# Data cleaning, handling missing values
+# Data management, dplyr
+#    subsetting
+#    aggregating
+#    joining, etc.
+
+ 
+ 
+#Functions (writing and using functions with parameters)
+
+ 
+#Plotting
+# base plot and ggplot (make sure you have the ggplot cheatsheet handy)
+
+
+#Univariate analysis
+#Bivariate analysis
+#Multivariate analysis 
+#Linear regression 
+#Logistic regression
+#Trees
+
+
+#R markdown creation
+
+
+#RShiny (Make sure you know how to use these widgets https://shiny.rstudio.com/gallery/widget-gallery.html)
